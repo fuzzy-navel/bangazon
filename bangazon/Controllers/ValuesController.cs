95095e1bda4eca56da728fb2b895e2cd7f10137d
Andy Million <amillion3@gmail.com>
2018-11-07 20:00:44 -0600

    Initial commit, creating the basic solution, no code added.

--- bangazon/Controllers/ValuesController.cs
+++ bangazon/Controllers/ValuesController.cs
@@ -17,6 +17,7 @@ namespace bangazon.Controllers
             return new string[] { "value1", "value2" };
         }
 
+
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
