dec5e4b20cea83f9210b9c9079f81ed17ac9d8b0
Alex Orga <alexorga.dev@gmail.com>
2018-11-10 17:11:25 -0600

    added Api get call that will return all of the product types and their id's

---
+++ bangazon/DataAccess/BangazonStorage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using bangazon.Models;
+
+namespace bangazon.DataAccess
+{
+    public class BangazonStorage
+    {
+        private const string ConnectionInfo = "Server = (local); Database=Bangazon; Trusted_Connection=True";
+
+        public List<ProductType> GetAllProductTypes()
+        {
+            using (var db = new SqlConnection(ConnectionInfo))
+            {
+                db.Open();
+
+                List<ProductType> ProductTypes = new List<ProductType>();
+
+                var command = db.CreateCommand();
+                command.CommandText = @"SELECT *
+                                        FROM product_types";
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var currentProductType = new ProductType()
+                    {
+                        category = reader["category"].ToString(),
+                        id = (int)reader["id"]
+                    };
+
+                    ProductTypes.Add(currentProductType);
+                }
+
+                return ProductTypes;
+            }
+        }
+    }
+}
