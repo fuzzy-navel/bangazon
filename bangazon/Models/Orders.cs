c57fda3ddeded08625935d43564dc250d37a09de
Nathan Pabst <nathan.pabst@gmail.com>
2018-11-10 18:44:30 -0600

    Orders class added to Models directory

---
+++ bangazon/Models/Orders.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bangazon.Models
+{
+    public class Orders
+    {
+        public int id { get; set; }
+        public int customer_id { get; set; }
+        public bool order_status { get; set; }
+        public bool can_complete { get; set; }
+        public int payment_type_id { get; set; }
+
+    }
+}
