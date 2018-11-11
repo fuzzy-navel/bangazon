8378fb803ea96d52596ea603545ebab3ef6d91a4
Nathan Pabst <nathan.pabst@gmail.com>
2018-11-11 12:29:37 -0600

    code for POST in place. need to confirm with db

--- bangazon/DataAccess/OrderStorage.cs
+++ bangazon/DataAccess/OrderStorage.cs
@@ -19,31 +19,38 @@ namespace bangazon.DataAccess
         {
             ConnectionString = config.GetSection("ConnectionString").Value;
         }
-        
+        // 1) GET ALL ORDERS
         public List<Order> GetOrders()
         {
             using (var connection = new SqlConnection(ConnectionString))
             {
                 connection.Open();
-
                 var result1 = connection.Query<Order>(@"SELECT * FROM orders");
-
                 return result1.ToList();
             }
         }
-
+        // 2) GET SINGLE ORDER BY ID
         public List<Order> GetOrderById(int id)
         {
             using (var connection = new SqlConnection(ConnectionString))
             {
                 connection.Open();
-
                 var result2 = connection.Query<Order>(@"SELECT * 
                                                        FROM orders as o
                                                        WHERE o.id = @id" 
                                                       );
                 return result2.ToList();
-
+            }
+        }
+        // 3) INSERT A NEW ORDER
+        public bool PostOrder(int customer_id, bool order_status, bool can_complete, int payment_type)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                string sql = "INSERT INTO order(CustomerId, OrderStatus, CanComplete, PaymentTypeId) VALUES(@CustomerId, @OrderStatus, @CanComplete, @PaymentTypeId)";
+                var result3 = connection.Execute(sql, new { CustomerId = customer_id, OrderStatus = order_status, CanComplete = can_complete, PaymentTypeId = payment_type });
+                return result3 == 1;
             }
         }
 
