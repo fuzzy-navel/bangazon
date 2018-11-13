55bee76828776de5193fe19868de3043fc4ee3bb
lolasimp <lolalsimp@gmail.com>
2018-11-12 19:42:47 -0600

    get all payment types

---
+++ bangazon/Controllers/PaymentTypeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bangazon.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bangazon.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentTypeController : ControllerBase
+    {
+        public PaymentStorage _storage;
+        public PaymentTypeController()
+        {
+            _storage = new PaymentStorage();
+        }
+
+        [HttpGet]
+        public IActionResult GetAllPaymentTypes()
+        {
+            return Ok(_storage.GetAllPaymentTypes());
+        }
+    }
+}
---
+++ bangazon/DataAccess/PaymentStorage.cs
@@ -0,0 +1,28 @@
+﻿using bangazon.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bangazon.DataAccess
+{
+    public class PaymentStorage
+    {
+        private const string ConnectionInfo = "Server = (local); Database=Bangazon; Trusted_Connection=True";
+
+       public IEnumerable<PaymentType>GetAllPaymentTypes()
+        {
+            using (var connection = new SqlConnection(ConnectionInfo))
+            {
+                connection.Open();
+                var result = connection.Query<PaymentType>(@"select * from payment_type");
+
+                return result;
+
+            }
+
+        }
+    }
+}
