32ffe315de6065497d217d044aeba6c68912b1e1
Barry Russell <blrussell09@gmail.com>
2018-11-13 20:12:40 -0600

    updated customer storage, connection string is now secure, and made changes to the getCustomerAndPayment() method; the method now returns the active and title colums from the database as well

---
+++ bangazon/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bangazon.Models
+{
+  public class Employee
+  {
+    public string name { get; set;}
+    public bool is_supervisor { get; set;}
+    public int department_id { get; set; }
+    public int id { get; set; }
+  }
+}
