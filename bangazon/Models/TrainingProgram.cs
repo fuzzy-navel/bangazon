087fcb74cc1f35b125850cec4394f3cf5de9e442
Andy Million <amillion3@gmail.com>
2018-11-13 22:15:44 -0600

    Setup TrainingProgram model

---
+++ bangazon/Models/TrainingProgram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bangazon.Models
+{
+    public class TrainingProgram
+    {
+        public DateTime Start_Date { get; set; }
+        public DateTime End_Date { get; set; }
+        public int Id { get; set; }
+    }
+}
