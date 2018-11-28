ddf4690dfbf987797b8888323ba6cf3a6f243dd6
Andy Million <amillion3@gmail.com>
2018-11-27 19:14:52 -0600

    Fixed a few routing issues, ready for review

--- WebApplication1/ClientApp/src/components/Header/Header.js
+++ WebApplication1/ClientApp/src/components/Header/Header.js
@@ -8,8 +8,16 @@ const Header = () => (
     <nav>
       <ul>
         <li><Link to='/'>Home</Link></li>
+        <li><Link to='/computers'>Computers</Link></li>
         <li><Link to='/customers'>Customers</Link></li>
+        <li><Link to='/departments'>Departments</Link></li>
         <li><Link to='/employees'>Employees</Link></li>
+        <li><Link to='/orders'>Orders</Link></li>
+        <li><Link to='/paymenttypes'>Payment Types</Link></li>
+        <li><Link to='/products'>Products</Link></li>
+        <li><Link to='/producttypes'>Product Types</Link></li>
+        <li><Link to='/trainingprograms'>Training Programs</Link></li>
+
       </ul>
     </nav>
   </header>
