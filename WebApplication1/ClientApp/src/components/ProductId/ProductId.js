1a657f6ef02cc799a6cd657ad3196f1c2bcf71b3
Andy Million <amillion3@gmail.com>
2018-11-27 21:06:12 -0600

    WIP, working on routing to a single product id

---
+++ WebApplication1/ClientApp/src/components/ProductId/ProductId.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+import './ProductId.css'
+
+class ProductId extends Component {
+  render () {
+    return (
+      <div></div>
+    );
+  }
+};
+
+export default ProductId;
--- WebApplication1/ClientApp/src/components/Products/Products.js
+++ WebApplication1/ClientApp/src/components/Products/Products.js
@@ -1,11 +1,62 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
-import './Products.css'
+import constants from '../../constants';
+import './Products.css';
 
+// http://localhost:58050/
 class Products extends Component {
+  state = {
+    products: [],
+  }
+
+  componentDidMount() {
+    const apiPath = `api/product`;
+    return new Promise((resolve, reject) => {
+      axios.get(apiPath)
+      .then(products => {
+        // sets state with all products
+        this.setState({
+          products: products.data
+        })
+        resolve (products);
+      })
+      .catch(error => reject(error));
+    });
+  };
+
   render () {
+    const {
+      products,
+      singleProduct
+    } = this.state;
+
+    const clickProduct = () => {
+
+
+      this.props.history.push(`/products/${singleProduct.id}`);
+    }
+
+    const output = products.map(product => {
+      // Prints all product titles to DOM
+      return (
+        <h4 key={product.id}
+            onClick={clickProduct} >
+          {product.title}
+        </h4>
+      );
+    });
+
     return (
-      <div></div>
+      <div>
+        <h1>Products</h1>
+        <div>
+          <h2>All Products:</h2>
+          <div>
+            {output}
+          </div>
+        </div>
+      </div>
     );
   }
 };
