8cc363ed07226f24ddcc0a72c077e5b0e30e2533
Nathan Pabst <nathan.pabst@gmail.com>
2018-11-28 17:04:32 -0600

    orders wip

--- WebApplication1/ClientApp/src/components/Orders/Orders.js
+++ WebApplication1/ClientApp/src/components/Orders/Orders.js
@@ -1,13 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import './Orders.css'
+import './Orders.css';
 
 class Orders extends Component {
-  render () {
-    return (
-      <div></div>
-    );
-  }
-};
+    state = {
+        orders: []
+    };
 
-export default Orders;
+    render() {
+        const { orders } = this.state;
+        const orderComponents = orders.map((order) => (
+            <div key={order.id}>
+                <table className="table table-bordered table-striped">
+                    <tbody>
+                        <tr>
+                            <td>{order.customerId}</td>
+                            <td>{order.orderStatus}</td>
+                            <td>{order.canComplete}</td>
+                            <td>{order.paymentTypeId}</td>
+                            <td>
+                                <button className="btn btn-primary"> Placeholder </button>  
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            
+            ));
+
+        return (
+            <div className="orders">
+                <div className="panel panel-primary">
+                    <div className="panel-heading">Order Management</div>
+                    <div className="panel-body">
+                        <ul className="orderComp">{orderComponents}</ul>
+                    </div>
+                </div>
+            </div>
+        );
+    };
+}
+
+export default Orders; 
