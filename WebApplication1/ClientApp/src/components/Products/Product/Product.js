6a3d99844038647bded2aaf2680d2bbda8ee82d2
Andy Million <amillion3@gmail.com>
2018-11-28 20:15:24 -0600

    WIP, get single product works!

--- WebApplication1/ClientApp/src/components/Products/Product/Product.js
+++ WebApplication1/ClientApp/src/components/Products/Product/Product.js
@@ -5,36 +5,77 @@ import './Product.css';
 
 class Product extends Component {
   state = {
-    product: {}
+    product: []
   }
 
   componentDidMount() {
-    // grabs the id from the URL
-    const id = (this.props.match.params.number) * 1;
-    const apiPath = `api/product/${id}`;
+    const id = this.props.match.params;
+    const apiPath = `api/product/${id.id}`;
     return new Promise((resolve, reject) => {
       axios
       .get(apiPath)
       .then(product => {
+        // sets state with single product
         this.setState({
-          product: product
+          product: product.data[0]
         })
-      })
-      .then(product => {
-        resolve(product);
+        resolve (product);
       })
       .catch(error => reject(error));
     });
   };
 
   render () {
+    const {product} = this.state;
     return (
       <div>
-        <p>hi</p>
+        <form>
+          <label>Title: </label>
+          <input
+            type="text"
+            name="product-name"
+            value={product.title}
+          /><br/>
+          <label>Category: </label>
+          <input
+            type="number"
+            name="product-category"
+            value={product.category}
+          /><br/>
+          <label>Description: </label>
+          <input
+            type="text"
+            name="product-description"
+            value={product.description}
+          /><br/>
+          <label>Price: </label>
+          <input
+            type="number"
+            name="product-price"
+            value={product.price}
+          /><br/>
+        <label>Quantity: </label>
+          <input
+            type="number"
+            name="product-quantity"
+            value={product.quantity}
+            /><br/>
+        <label>Owner Id: </label>
+          <input
+            type="number"
+            name="product-owner-id"
+            value={product.owner_id}
+          /><br/>
+        <label>Id: </label>
+          <input
+            type="number"
+            name="product-id"
+            value={product.id}
+          /><br/>
+        </form>
       </div>
     );
   }
-
 };
 
 export default Product;
