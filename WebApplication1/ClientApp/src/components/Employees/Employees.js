80807d3298c10c21b1232841f799bd423e9f259c
Barry Russell <blrussell09@gmail.com>
2018-12-01 10:00:10 -0600

    stashing changes

--- WebApplication1/ClientApp/src/components/Employees/Employees.js
+++ WebApplication1/ClientApp/src/components/Employees/Employees.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react';
-
+import employeeRequests from '../../EmployeeRequests/employeeRequests';
 import './Employees.css'
 
-class Employees extends Component {
-  render () {
+class Employees extends Component
+{
+  state =
+  {
+    employees: [],
+  }
+
+  componentDidMount()
+  {
+    employeeRequests.getEmployees()
+      .then((res) =>
+      {
+        this.setState({employees: res})
+      })
+      .catch((err) =>
+      {
+        console.error(err);
+      });
+  };
+
+  render()
+  {
     return (
       <div></div>
     );
