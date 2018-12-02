63d60086f7bb68f84ea1dc03ad9358d4a4972180
Andy Million <amillion3@gmail.com>
2018-12-02 11:12:41 -0600

    WIP, get all in progress. need to format data into a table

---
+++ WebApplication1/ClientApp/src/components/Computers/AllComputers/AllComputers.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+
+import Requests from '../Requests/Requests';
+
+import './AllComputers.css';
+
+class AllComputers extends Component {
+  state = {
+    allComputers: [],
+  }
+
+  componentDidMount() {
+    return new Promise((resolve, reject) => {
+      Requests.GetAll()
+      .then(computer => {
+        this.setState({
+          allComputers: computer,
+        });
+        resolve(computer);
+      })
+      .catch(error => reject(error));
+    });
+  };
+
+  clickAllComputers = e => {
+    const id = e.target.name;
+    this.props.history.push(`/computers/${id}`);
+  };
+
+  render () {
+    const { allComputers } = this.state;
+
+    const output = allComputers.map(c => {
+      return (
+        <Button
+          name={c.id}
+          key={c.id}
+          value={c.id}
+          onClick={this.clickAllComputers}
+        ></Button>
+      );
+    });
+
+    return (
+      <div>
+        {output}
+      </div>
+    );
+  }
+};
