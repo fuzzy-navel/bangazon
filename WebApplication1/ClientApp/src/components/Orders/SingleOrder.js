474ad906e6dd4f7ef568d2212d6ec231f32455c2
Nathan Pabst <nathan.pabst@gmail.com>
2018-12-02 14:44:16 -0600

    completed SingleOrder class

--- WebApplication1/ClientApp/src/components/Orders/SingleOrder.js
+++ WebApplication1/ClientApp/src/components/Orders/SingleOrder.js
@@ -5,41 +5,84 @@ import orderRequests from '../../OrderRequests/OrderRequests';
 
 class SingleOrder extends Component {
     state = {
-        customerId: 0,
-        orderStatus: false,
-        canComplete: false,
-        paymentTypeId: 0,
-        id: 0
+        showEditForm: false,
+        order: {
+            customerId: 0,
+            orderStatus: false,
+            canComplete: false,
+            paymentTypeId: 0,
+            id: 0
+        },
+    };
+
+    //**************CAPTURES NEW USER INPUT WITHIN THE EDIT FORM************//
+    handleInputChange = (e) => {
+        const orderStatus = e.target.value;
+        const canComplete = e.target.value;
+        const paymentTypeId = e.target.value;
+        this.setState({
+            order: {
+                ...this.state.order,
+                orderStatus,
+                canComplete,
+                paymentTypeId
+            }
+        });
     }
 
-    componentDidMount() {
-        orderRequests.getOrderById()
-            .then((order) => {
-                this.setState({
-                    customerId,
-                    orderStatus,
-                    canComplete,
-                    paymentTypeId,
-                    id
-                })
-                resolve(order);
-            })
-            .catch((error) => {
-                console.error('error retrieving orders', error);
+    //********************SHOW THE EDIT FORM ON BUTTON CLICK**************//
+    toggleShowEditForm = () => {
+        this.setState({
+            showEditForm: !this.state.showEditForm,
+            order: { ...this.props.order },
+        });
+    }
+
+    handleSave = () => {
+        this.props.handleSaveClickEvent(this.props.order.id, this.state.order)
+            .then(() => {
+                this.toggleShowEditForm();
             });
     }
-
+    
     render() {
+        const { order } = this.props;
 
         return (
-            <div>
-                <h2> Order: {this.state.id} </h2>
-                <p>Customer Number: {this.state.customerId}</p>
-                <p>Order Status: {this.state.orderStatus}</p>
-                <p>Order Complete: {this.state.canComplete}</p>
-                <p>Payment Type Number{this.state.paymentTypeId}</p>
+            <div className="col-sm-4 col-med-2" key={order.id}>
+                <div className="thumbnail">
+                    <div className="caption">
+                        <h3> Order Number: {this.order.id}</h3>
+                        <p>Customer Number: {this.order.customerId}</p>
+                        <p>Order Status: {this.order.orderStatus}</p>
+                        <p>Order Complete: {this.order.canComplete}</p>
+                        <p>Payment Type Number: {this.order.paymentTypeId}</p>
+                        <p><button type="button" className="btn btn-primary" id={order.id} onClick={this.toggleShowEditForm}>Edit Details</button></p>
+                    </div>
+                    <div className={this.state.showEditForm ? '' : 'hide'}>
+                        <input
+                            type="text"
+                            onChange={this.handleInputChange}
+                            value={this.state.order.orderStatus}
+                            className="form-control"
+                        />
+                        <input
+                            type="text"
+                            onChange={this.handleInputChange}
+                            value={this.state.order.canComplete}
+                            className="form-control"
+                        />
+                        <input
+                            type="text"
+                            onChange={this.handleInputChange}
+                            value={this.state.order.paymentTypeId}
+                            className="form-control"
+                        />
+                        <button className="btn btn-default" type="button" onClick={this.handleSave}>Save</button>
+                    </div>
+                </div>
             </div>
-            ); 
+         ); 
     }
 }
 
