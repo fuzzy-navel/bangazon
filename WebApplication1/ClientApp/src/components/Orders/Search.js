a4d2451895d2cb86569ba289ec31c4dcb78838c5
Nathan Pabst <nathan.pabst@gmail.com>
2018-12-02 13:21:08 -0600

    updated Search class

--- WebApplication1/ClientApp/src/components/Orders/Search.js
+++ WebApplication1/ClientApp/src/components/Orders/Search.js
@@ -1,1 +1,32 @@
-﻿
+﻿import React from 'react';
+
+class Search extends React.Component {
+    state = {
+        searchInput: 0
+    }
+
+    handleInputChange = () => {
+        this.props.onSearch(
+            this.search.value,
+        );
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col-sm-6">
+                    <div className="input-group searchBar">
+                        <input
+                            type="text" className="form-control"
+                            placeholder="Search by order number..."
+                            ref={input => this.search = input}
+                            onChange={this.handleInputChange}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Search;
