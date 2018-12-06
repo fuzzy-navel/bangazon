7df4a823f753c22221018e90246745a597646dff
Barry Russell <blrussell09@gmail.com>
2018-12-05 20:03:39 -0600

    users will now be shown a success message on a successful update, added a method to validate the forms before sending

--- WebApplication1/ClientApp/src/components/SingleEmployee/SingleEmployee.js
+++ WebApplication1/ClientApp/src/components/SingleEmployee/SingleEmployee.js
@@ -62,6 +62,44 @@ class SingleEmployee extends Component
       this.setState({ updatedEmployee: tempEmp });
     };
 
+    const fieldCheckr = () =>
+    {
+      const updatedEmp = this.state.employee[0];
+      const empName = updatedEmp.employee_name;
+      const depId = updatedEmp.department_id;
+      const isSup = updatedEmp.supervisor;
+      var fieldsOk = 0;
+      switch (empName)
+      {
+        case "":
+          alert("The employee name field is empty.");
+          break;
+        default:
+          fieldsOk++
+      }
+      switch (depId)
+      {
+        case 0:
+          alert("The department id cannot be 0.");
+          break;
+        default:
+          fieldsOk++
+      }
+      switch (isSup)
+      {
+        case "true":
+        case "false":
+          fieldsOk++;
+          break;
+        default:
+          alert("Is supervisor field must be either true or false (lowercase).");
+      }
+      if (fieldsOk === 3)
+      {
+        updateEmployee();
+      }
+    };
+
     const updateEmployee = () =>
     {
       const updatedEmp = this.state.employee[0];
@@ -69,7 +107,11 @@ class SingleEmployee extends Component
       updatedEmp.is_supervisor = updatedEmp.supervisor;
       updatedEmp.name = updatedEmp.employee_name
       employeeRequests.updateEmployee(id, updatedEmp)
-        .then()
+        .then(() =>
+        {
+          alert("Employee successfully updated!")
+          this.props.history.push(`/`);
+        })
         .catch((err) =>
         {
           console.error(err);
@@ -101,29 +143,13 @@ class SingleEmployee extends Component
             <h3 className={this.state.class === 'hidden' ? 'hidden' : ''}>Department ID</h3>
             <input className={this.state.class === 'hidden' ? 'hidden' : ''} id="departmentId" onChange={employeeDepIdChange} value={employee.department_id} />
             <h3 className={this.state.class === 'hidden' ? 'hidden' : ''}>Is Supervisor?</h3>
-            <input className={this.state.class === 'hidden' ? 'hidden' : ''} id="isSupervisor" onChange={employeeSupervisorChange} />
+            <input className={this.state.class === 'hidden' ? 'hidden' : ''} id="isSupervisor" onChange={employeeSupervisorChange} placeholder={employee.is_supervisor.toString()}/>
             <h3> </h3>
-            <button className={this.state.class === 'hidden' ? 'hidden btn btn-primary' : 'btn btn-primary'} onClick={updateEmployee}>Submit</button>
+            <button className={this.state.class === 'hidden' ? 'hidden btn btn-primary' : 'btn btn-primary'} onClick={fieldCheckr}>Submit</button>
           </div>
         </div>
       )
     });
-
-    //const editEmployee = this.state.employee.map((employee) =>
-    //{
-    //  return (
-    //    <div className="col-md-3 col-md-offset-3" key={employee.employee_id}>
-    //      <h3 className={this.state.class === 'hidden' ? 'hidden' : ''} >Name</h3>
-    //      <input className={this.state.class === 'hidden' ? 'hidden' : ''} id="employeeName" onChange={employeeNameChange}  value={employee.employee_name} />
-    //      <h3 className={this.state.class === 'hidden' ? 'hidden' : ''}>Department ID</h3>
-    //      <input className={this.state.class === 'hidden' ? 'hidden' : ''} id="departmentId" value={employee.department_id}/>
-    //      <h3 className={this.state.class === 'hidden' ? 'hidden' : ''}>Is Supervisor?</h3>
-    //      <input className={this.state.class === 'hidden' ? 'hidden' : ''} id="isSupervisor" value={employee.is_supervisor}/>
-    //      <h3> </h3>
-    //      <button className={this.state.class === 'hidden' ? 'hidden btn btn-primary' : 'btn btn-primary'} onClick={this.updateCustomer}>Submit</button>
-    //    </div>   
-    //  )
-    //});
 
     return (
       <div className="row">
