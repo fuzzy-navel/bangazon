36c9a534ec64d27bad79a300b8a9216dd222f126
Nathan Pabst <nathan.pabst@gmail.com>
2018-12-13 18:33:25 -0600

    fixed bug. GetOrderById working again

--- WebApplication1/DataAccess/OrderStorage.cs
+++ WebApplication1/DataAccess/OrderStorage.cs
@@ -46,7 +46,7 @@ namespace bangazon.DataAccess
                                                         OrderStatus = order_status, 
                                                         CanComplete = can_complete,
                                                         PaymentTypeId = payment_type_id, 
-                                                        OrderId = id 
+                                                        Id = id 
                                                       FROM orders as o               
                                                       WHERE o.id = @id",
                                                       new { id = id }
