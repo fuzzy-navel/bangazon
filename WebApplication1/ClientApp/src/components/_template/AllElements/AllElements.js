40b9e9e81a243b96fa275c92e24786b9af4a4cc5
Andy Million <amillion3@gmail.com>
2018-12-18 20:16:46 -0600

    Added boilerplate template for regular components

---
+++ WebApplication1/ClientApp/src/components/_template/AllElements/AllElements.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './AllElements.css';
+
+class AllElements extends Component {
+  render () {
+    return (
+      null
+    );
+  }
+};
+
+export default AllElements;
