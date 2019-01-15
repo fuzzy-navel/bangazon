1009c70be4478907bc0210d1d593c9c93f4cdfd9
Alex Orga <alexorga.dev@gmail.com>
2019-01-14 21:10:12 -0600

    WIP

--- bangazonUserFront/ClientApp/src/App.js
+++ bangazonUserFront/ClientApp/src/App.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
+import { BrowserRouter, Redirect, Switch } from 'react-router-dom';
+import firebase from 'firebase';
+import fbConnection from './firebaseRequests/connection';
+
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+fbConnection();
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                authed === true ? (
+                    <Component {...props} />
+                ) : (
+                        <Redirect
+                            to={{ pathname: '/login', state: { from: props.location } }}
+                        />
+                    )
+            }
+        />
+    );
+};
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                authed === false ? (
+                    <Component {...props} />
+                ) : (
+                        <Redirect
+                            to={{ pathname: '/orders', state: { from: props.location } }}
+                        />
+                    )
+            }
+        />
+    );
+};
 
 export default class App extends Component {
   displayName = App.name
