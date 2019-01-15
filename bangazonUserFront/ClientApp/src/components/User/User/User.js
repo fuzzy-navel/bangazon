32925b61f59f188752f3beae517c4a0bf999e6f1
Barry Russell <blrussell09@gmail.com>
2019-01-14 19:20:09 -0600

    Added user component

---
+++ bangazonUserFront/ClientApp/src/components/User/User/User.js
@@ -0,0 +1,16 @@
+﻿import React, { Component } from 'react';
+import './User.css';
+
+class User extends Component
+{
+  render()
+  {
+    return (
+      <div>
+        <h3>Poop</h3>
+      </div>
+    );
+  };
+};
+
+export default User;
---
+++ bangazonUserFront/ClientApp/src/components/User/Users.js
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import User from './User/User';
+
+const Users = () => (
+  <Switch>
+    <Route exact path='/user' component={User}/>
+  </Switch>
+)
+
+export default Users;
