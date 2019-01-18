ebaddd77ee275f2e6ca951017c1891cb9f94d1f0
lolasimp <lolalsimp@gmail.com>
2019-01-18 14:18:54 -0600

    working on adding payments

--- WebApplication1/ClientApp/src/constants.js
+++ WebApplication1/ClientApp/src/constants.js
@@ -1,7 +1,15 @@
 ﻿const constants = {
     bangazonConfig: {
-        dataBaseURL: 'https://localhost:50002'
-    }
+        dataBaseURL: 'https://localhost:44352',
+    },
+    firebaseConfig: {
+        apiKey: "AIzaSyCJcEAhXmr5toJaiSORDBcGDjJvHuH4ce8",
+        authDomain: "bangazon-5b271.firebaseapp.com",
+        databaseURL: "https://bangazon-5b271.firebaseio.com",
+        projectId: "bangazon-5b271",
+        storageBucket: "bangazon-5b271.appspot.com",
+        messagingSenderId: "649759244979",
+    },
 };
 
 export default constants;
--- bangazonUserFront/ClientApp/src/components/PaymentType/AddPaymentType/AddPaymentType.js
+++ bangazonUserFront/ClientApp/src/components/PaymentType/AddPaymentType/AddPaymentType.js
@@ -1,12 +1,60 @@
 ﻿import React, { Component } from 'react';
+import { form, label, button, FormControl } from 'react-bootstrap';
 import '../PaymentType.css';
 
 class AddPaymentType extends Component {
+    state = {
+        account_number: 0,
+        title: '',
+        customer_id: 0,
+        active: false,
+        id: 0,
+    }
+
+    handleChange = e => {
+        const { name, value } = e.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
     render() {
+        const { account_number } = this.state;
+        const { title } = this.state;
+        const { customer_id } = this.state;
+        const { active } = this.state;
+        const { id } = this.state;
+
         return (
-            <div className="AddPay">
-                <h2>ADDPAYMENT</h2>
-            </div>
+            
+            <form>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <label for="account_number">Account Number</label>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <FormControl type="number" name="account_number" value={this.state.account_number} onChange={this.handleChange}/>
+                    </div>
+                </div>       
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label for="inputEmail4">Card Type</label>
+                        </div>
+                        <div className="form-group col-md-6">
+                        <label for="inputPassword4"></label>
+                        <FormControl type="name" value={this.state.title} onChange={this.handleChange} className="form-control" id="cardtype"/>
+                        </div>
+                  </div>       
+                 <div className="form-group">
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" id="gridCheck"></input>
+                        <label className="form-check-label" for="gridCheck">
+                            Check if all information is correct
+                        </label>
+                     </div>
+                 </div>
+                   <button type="submit" className="btn btn-primary">Save Payment</button>
+               </form>
         );
     }
 }
