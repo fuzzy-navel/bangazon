16acfd1b175b38e93f8918768f0c7cda3665484e
Andy Million <amillion3@gmail.com>
2019-01-17 20:00:56 -0600

    Added a filter products method. It is basic, more can be added later

---
+++ bangazonUserFront/ClientApp/src/components/Shop/FilterProducts/FilterProducts.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import { Checkbox, Col, ControlLabel, Label } from "react-bootstrap";
+
+import Requests from "../../Requests/ProductRequests";
+
+class FilterProducts extends Component {
+  state = {
+    inUse: false
+  };
+
+  toggleCheckbox = e => {
+    const { name } = e.target;
+    this.props.callbackFromParent(!this.state.inUse);
+    this.setState({ [name]: !this.state[name] });
+  };
+
+  render() {
+    return (
+      <Col xs={12}>
+        <Label>In Stock Only? </Label>
+        <Checkbox
+          name="inUse"
+          checked={this.state.inUse}
+          onChange={this.toggleCheckbox}
+        />
+        <br />
+      </Col>
+    );
+  }
+}
+
+export default FilterProducts;
